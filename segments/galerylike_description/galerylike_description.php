9dde6334a7ec8559ed6a5a037b6d375a707be3e1
dev0ps221 <seybalofficiel@gmail.com>
2023-04-21 23:23:56 +0000

    main structure:galerylike_description:galerylike_picture

--- segments/galerylike_description/galerylike_description.php
+++ segments/galerylike_description/galerylike_description.php
@@ -1,22 +1,30 @@
+<link rel="stylesheet" href="/assets/css/galerylike_description.css">
 <div class="galerylike_description">
     <div class="galerylike_pictures_wrapper">
-
+        <div class="galerylike_pictures">
+            <img src="/assets/pictures/persons.avif" alt="person" class="galerylike_picture">
+            <img src="/assets/pictures/girl.avif" alt="person" class="galerylike_picture">
+            <img src="/assets/pictures/person.avif" alt="person" class="galerylike_picture">
+            <img src="/assets/pictures/guy.avif" alt="person" class="galerylike_picture">
+        </div>
     </div>
     <div class="galerylike_description_description_wrapper">
-        <div class="galerylike_description_description_titre">
-            <h1>
-                titre 
-            </h1>
-        </div>
-        <div class="galerylike_description_description_texte">
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae reprehenderit totam vitae in possimus maiores modi deleniti
-            </p>
-        </div>
-        <div class="galerylike_description_description_action_wrapper">
-            <a href="" class='galerylike_description_description_action'>
-                en savoir plus
-            </a>
+        <div class="galerylike_description_description">
+            <div class="galerylike_description_description_titre">
+                <h1>
+                    titre 
+                </h1>
+            </div>
+            <div class="galerylike_description_description_texte">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae reprehenderit totam vitae in possimus maiores modi deleniti
+                </p>
+            </div>
+            <div class="galerylike_description_description_action_wrapper">
+                <a href="" class='galerylike_description_description_action'>
+                    en savoir plus
+                </a>
+            </div>
         </div>
     </div>
 </div>
