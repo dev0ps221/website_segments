2b762c79c2aaffa8df83c9d9e900e70ff786b517
dev0ps221 <seybalofficiel@gmail.com>
2023-04-23 15:21:21 +0000

    picture_portfolio

--- segments/picture_portfolio/picture_portfolio.php
+++ segments/picture_portfolio/picture_portfolio.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="/assets/css/picture_portfolio.css">
 <div id="picture_portfolio">
     <div class="picture_portfolio_heading_wrapper">
         <h1 class="picture_portfolio_heading_title">
@@ -17,6 +18,30 @@
             </div>
             <div class="picture_portfolio_portfolio_picture_wrapper">
                 <img src="/assets/pictures/girl.avif" alt="image portfolio" class="picture_portfolio_portfolio_picture">
+            </div>
+            <div class="picture_portfolio_portfolio_picture_wrapper">
+                <img src="/assets/pictures/persons.avif" alt="image portfolio" class="picture_portfolio_portfolio_picture">
+            </div>
+            <div class="picture_portfolio_portfolio_picture_wrapper">
+                <img src="/assets/pictures/persons.avif" alt="image portfolio" class="picture_portfolio_portfolio_picture">
+            </div>
+            <div class="picture_portfolio_portfolio_picture_wrapper">
+                <img src="/assets/pictures/guy.avif" alt="image portfolio" class="picture_portfolio_portfolio_picture">
+            </div>
+            <div class="picture_portfolio_portfolio_picture_wrapper">
+                <img src="/assets/pictures/persons.avif" alt="image portfolio" class="picture_portfolio_portfolio_picture">
+            </div>
+            <div class="picture_portfolio_portfolio_picture_wrapper">
+                <img src="/assets/pictures/person.avif" alt="image portfolio" class="picture_portfolio_portfolio_picture">
+            </div>
+            <div class="picture_portfolio_portfolio_picture_wrapper">
+                <img src="/assets/pictures/girl.avif" alt="image portfolio" class="picture_portfolio_portfolio_picture">
+            </div>
+            <div class="picture_portfolio_portfolio_picture_wrapper">
+                <img src="/assets/pictures/persons.avif" alt="image portfolio" class="picture_portfolio_portfolio_picture">
+            </div>
+            <div class="picture_portfolio_portfolio_picture_wrapper">
+                <img src="/assets/pictures/guy.avif" alt="image portfolio" class="picture_portfolio_portfolio_picture">
             </div>
             <div class="picture_portfolio_portfolio_picture_wrapper">
                 <img src="/assets/pictures/persons.avif" alt="image portfolio" class="picture_portfolio_portfolio_picture">
